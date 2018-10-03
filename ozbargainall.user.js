53ac857d38be69cd619f913fbe62f4104811e817
Jimmy Flip <jimmy@jiimyflip.com>
2018-10-03 22:44:39 +1000

    Added script to show/hide all replies

---
+++ ozbargainall.user.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name         OzBargain Hide All Comment Replies
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Hide all comment replies on OzBargain
+// @author       Dazweeja
+// @match        https://www.ozbargain.com.au/node/*
+// @grant        none
+// @require  https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    var replies = $('ul.comment.level1');
+    var showLink = $('<a>show replies</a>').click(function() {
+        if ($( this ).text() == 'show replies') {
+            replies.css('display', 'block');
+            $( this ).text('hide replies');
+        }
+        else {
+            replies.css('display', 'none');
+            $( this ).text('show replies');
+        }
+    });
+
+    var item = $('<span>').append(' (').append(showLink).append(' )');
+    $('#comment').append(item);
+})();
